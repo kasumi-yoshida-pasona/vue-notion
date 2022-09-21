0e94d526fc1d51f23851c7282bcc9faccbd784e2
kasumi-yoshida-pasona <110513233+kasumi-yoshida-pasona@users.noreply.github.com>
2022-09-21 15:27:41 +0900

    枠組み作成＋各種ライブラリ導入

---
+++ src/components/MainPage.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="main-page">
+    メインページ
+  </div>
+</template>
+
+<script>
+  export default {}
+</script>
+
+<style scope>
+  .main-page {
+    padding: 10px;
+  }
+</style>
